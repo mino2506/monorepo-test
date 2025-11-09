bf54cc12733009c23a6630632642bbdd1e501558
liones_2612 <liones2612@gmail.com>
2025-11-09 12:35:26 +0900

    Add: logging and error handling

--- apps/rust-server/src/main.rs
+++ apps/rust-server/src/main.rs
@@ -1,29 +1,45 @@
 use axum::{routing::{get, post}, Json, Router};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
-// use tracing_subscriber::EnvFilter;
-
+use tower_http::trace::TraceLayer;
+use tracing;
+use tracing_subscriber::EnvFilter;
+use dotenvy::dotenv;
 #[tokio::main]
 async fn main() {
+    if let Err(e) = run().await {
+        eprintln!("fatal error: {e}");
+        std::process::exit(1);
+    }
+}
+
+async fn run()-> Result<(), Box<dyn std::error::Error>> {
+    // .env読み込み
+    dotenv().ok();
+
     // ログ初期化（最低限）
-    // tracing_subscriber::fmt()
-    //     .with_env_filter(EnvFilter::from_default_env())
-    //     .init();
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    tracing::info!("start Logging successfully");
 
     // ルータ定義
     let app: Router = Router::new()
-      .route("/health", get(health))
-      .route("/hello", get(hello))
-      .route("/echo", post(echo));
+        .route("/health", get(health))
+        .route("/hello", get(hello))
+        .route("/echo", post(echo))
+        .layer(TraceLayer::new_for_http());
 
     // アドレス
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::info!("listening on http://{}", addr);
 
     // 起動
-    axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app)
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
 
 
